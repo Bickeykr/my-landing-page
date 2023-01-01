78a4122a1a7c14e59f10fb7877c888029d24a6f5
Bickeykr <79741948+Bickeykr@users.noreply.github.com>
2023-01-01 11:49:33 +0530

    01.01.2023 Worked a bit on nav bar

---
+++ src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+    return (
+        <div>
+            <Navbar/> 
+            App
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CircleLogo from '../images/SmartRuppeLogo.png';
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <img src={CircleLogo} alt="SmartRuppeLogo" />
+                <h1>Text for you</h1>
+            </nav>
+            </div>
+
+    )
+}
+
+export default Navbar;
